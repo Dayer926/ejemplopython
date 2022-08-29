cec1b5ba89ee49496c25df3bce3d9c9593dd6722
dayer <dayer.torres14473@est.uca.edu.ni>
2022-08-29 09:55:56 -0600

    Ejemplo 1 arreglo

---
+++ Arreglo1.py
@@ -0,0 +1,10 @@
+numero = []
+numeros.append(3)
+numeros.append(8)
+numeros.append(9)
+numeros.append(20)
+print("El numero de arreglo")
+print(numeros)
+numeros.insert(1, 15)
+print(numeros)
+numero.remove 
