ff225b32d4c73f7a99e86b23e25767ee626fd414
dayer <dayer.torres14473@est.uca.edu.ni>
2022-08-29 10:31:29 -0600

    Agregue 2 ejercicios mas

---
+++ Arreglo2.py
@@ -0,0 +1,15 @@
+
+
+
+listaEst = []
+resp = "Si"
+while(resp.upper != "NO"):
+    tam = len(listaEst)
+    print("Elementos guardados: ", tam)
+    nomnres = input("Escriba nombre del estudiante: ")
+    listaEst.append(nombres)
+     resp = input("Desea agregar mas? SI - NO: ")
+
+for est in listaEst:
+    print(est)
+
